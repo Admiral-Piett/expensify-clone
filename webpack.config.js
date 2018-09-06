b5a7e6fedc39e04b0687e85ab8e87817f6836ec7
Devin Humphreys <humphred@advisory.com>
2018-09-06 14:07:30 -0400

    dependencies clean up

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
         mode: isProduction ? 'production' : 'development',
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         // loader - customize how to load a file when it sees it
@@ -51,7 +51,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 }
