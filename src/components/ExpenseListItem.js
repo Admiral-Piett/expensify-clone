f8295f121f0f5cd62ad7eba1d991c40ca8d0d5f3
Devin Humphreys <humphred@advisory.com>
2018-09-06 14:55:01 -0400

    updating format of amount and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 import { removeExpense } from '../actions/expenses';
 
@@ -10,7 +11,8 @@ export const ExpenseItem = (props) => (
         <Link to={`edit/${props.id}`}>
             <h3>{props.description}</h3>
         </Link>
-        <p>Cost: ${props.amount}, Created On: {props.createdAt}</p>
+        <p>Cost: {numeral(props.amount / 100).format('$0,0.00')}</p> 
+        <p>Created On: {moment(props.createdAt).format('MMMM Do, YYYY')}</p>
         <Link to={`edit/${props.id}`}>Edit</Link>
     </div>
 );
